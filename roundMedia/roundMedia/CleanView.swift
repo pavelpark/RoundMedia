93a373097a7a1371d99be1af5c5e2184e97015b3
Pavel Parkhomey <pavelpark@aol.com>
2017-07-14 18:38:59 -0700

    New CocoTouchClass added "View"

---
+++ roundMedia/roundMedia/CleanView.swift
@@ -0,0 +1,15 @@
+//
+//  CleanView.swift
+//  roundMedia
+//
+//  Created by Pavel Parkhomey on 7/14/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+
+class CleanView: UIView {
+
+
+
+}
