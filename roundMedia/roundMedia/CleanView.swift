cb7e9716826d02b3adc0f386eb1f79d8a4b9ebdb
Pavel Parkhomey <pavelpark@aol.com>
2017-07-14 18:54:07 -0700

    Created a shadow for logo View.

--- roundMedia/roundMedia/CleanView.swift
+++ roundMedia/roundMedia/CleanView.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class CleanView: UIView {
-
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
 
 }
---
+++ roundMedia/roundMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  roundMedia
+//
+//  Created by Pavel Parkhomey on 7/14/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
