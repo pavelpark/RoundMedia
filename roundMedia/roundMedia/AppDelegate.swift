303357243186ab71260ec8e4d89e0f7629ba3693
Pavel Parkhomey <pavelpark@aol.com>
2017-07-17 14:03:40 -0700

    Open facebook url in AppDelegate.

--- roundMedia/roundMedia/AppDelegate.swift
+++ roundMedia/roundMedia/AppDelegate.swift
@@ -47,7 +47,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let sourceApplication: String? = options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: sourceApplication, annotation: nil)
+        
+    }
 
 }
 
