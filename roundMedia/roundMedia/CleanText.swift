404919ac420f6a83e2cb50e831ba0a5d7b26298c
Pavel Parkhomey <pavelpark@aol.com>
2017-07-17 12:14:21 -0700

    Corner Radius added around the text fields.

--- roundMedia/roundMedia/CleanText.swift
+++ roundMedia/roundMedia/CleanText.swift
@@ -15,6 +15,7 @@ class CleanText: UITextField {
         
         layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.1).cgColor
         layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
     }
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
