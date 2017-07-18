6c8147d81438c37174a0f877df1134c476430710
Pavel Parkhomey <pavelpark@aol.com>
2017-07-18 00:58:28 -0700

    Facebook Credential all setup.

--- roundMedia/roundMedia/SignInViewController.swift
+++ roundMedia/roundMedia/SignInViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInViewController: UIViewController {
 
@@ -22,6 +25,26 @@ class SignInViewController: UIViewController {
 
     @IBAction func facebookButtonTapped(_ sender: AnyObject) {
         
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("------: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("------: User cancelled Facebook authentication")
+            } else {
+                print("------: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("------: Unable to authenticate with Firebase - \(String(describing: error))")
+            }
+        }
     }
 
 }
