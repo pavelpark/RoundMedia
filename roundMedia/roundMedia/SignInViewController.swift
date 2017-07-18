5ad83cfde2d73f09a5e15693890cdc8987b8f746
Pavel Parkhomey <pavelpark@aol.com>
2017-07-18 00:31:24 -0700

    Facebook button action outlet connected.

--- roundMedia/roundMedia/SignInViewController.swift
+++ roundMedia/roundMedia/SignInViewController.swift
@@ -20,6 +20,9 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: AnyObject) {
+        
+    }
 
 }
 
