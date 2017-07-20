0577e1452cbd43ef10bfbab0206efb53c5c55290
Pavel Parkhomey <pavelpark@aol.com>
2017-07-19 18:37:11 -0700

    Cleaned up SignInViewController.

--- roundMedia/roundMedia/SignInViewController.swift
+++ roundMedia/roundMedia/SignInViewController.swift
@@ -22,10 +22,7 @@ class SignInViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
     @IBAction func facebookButtonTapped(_ sender: AnyObject) {
         
