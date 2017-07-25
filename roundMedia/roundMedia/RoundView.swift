8b19caecdb9c39299f36192efa69c049262e50e9
Pavel Parkhomey <pavelpark@aol.com>
2017-07-25 13:40:46 -0700

    Added Images for buttons and created a new View "RoundView'

---
+++ roundMedia/roundMedia/RoundView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundView.swift
+//  roundMedia
+//
+//  Created by Pavel Parkhomey on 7/25/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+
+class RoundView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
