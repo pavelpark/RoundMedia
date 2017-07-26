43368b11c1c48df8ed89d8b38096d4afab8594ce
Pavel Parkhomey <pavelpark@aol.com>
2017-07-26 15:32:06 -0700

    Added 5 IBOutlets to PostCell.

--- roundMedia/roundMedia/PostCell.swift
+++ roundMedia/roundMedia/PostCell.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class PostCell: UITableViewCell {
 
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var LikesLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
