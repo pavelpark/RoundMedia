f26d2719a439420daba5e9bd7dd787ba7c4e0999
Pavel Parkhomey <pavelpark@aol.com>
2017-07-26 15:25:39 -0700

    Created a postCell View.

---
+++ roundMedia/roundMedia/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  roundMedia
+//
+//  Created by Pavel Parkhomey on 7/26/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
