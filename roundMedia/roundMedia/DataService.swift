c8d8a4781bfc90a54c709f99d5b64650f7983491
Pavel Parkhomey <pavelpark@aol.com>
2017-07-27 13:34:14 -0700

    Created REF Points for Base, Posts, Users.

--- roundMedia/roundMedia/DataService.swift
+++ roundMedia/roundMedia/DataService.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    static let ds = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_POSTS: DatabaseReference {
+        return _REF_POSTS
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+}
