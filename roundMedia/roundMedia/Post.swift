df57385eb6d28beba2a33c660fcf665ae881096a
Pavel Parkhomey <pavelpark@aol.com>
2017-07-28 15:39:34 -0700

    Assigned each Post to its self.

--- roundMedia/roundMedia/Post.swift
+++ roundMedia/roundMedia/Post.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    var imageUrl: String {
+        return _imageUrl
+    }
+    var likes: Int {
+        return _likes
+    }
+    var postKey: String {
+        return _postKey
+    }
+    
+    
+    
+}
