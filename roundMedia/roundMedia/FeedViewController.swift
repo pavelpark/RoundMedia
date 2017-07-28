ca6713ab77a418a951a84854df02f65a7f474e20
Pavel Parkhomey <pavelpark@aol.com>
2017-07-28 14:17:26 -0700

    Receiving all the JSON successfully from Firebase.com

--- roundMedia/roundMedia/FeedViewController.swift
+++ roundMedia/roundMedia/FeedViewController.swift
@@ -20,7 +20,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
-        DataService.ds.REF_POSTS
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value)
+        })
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
