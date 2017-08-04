e5194aa297d02512393dbf2e19479a8b31a312ad
Pavel Parkhomey <pavelpark@aol.com>
2017-08-04 13:15:17 -0700

    Created a new var for Current User.

--- roundMedia/roundMedia/DataService.swift
+++ roundMedia/roundMedia/DataService.swift
@@ -35,6 +35,11 @@ class DataService {
     var REF_USERS: DatabaseReference {
         return _REF_USERS
     }
+    var REF_USER_CURRENT: DatabaseReference{
+        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
+        let user = REF_USERS.child(uid!)
+        return user
+    }
     var REF_POST_IMAGES: StorageReference {
         return _REF_POST_IMAGES
     }
